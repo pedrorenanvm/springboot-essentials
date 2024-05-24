72bd9a3de1ef60c1dd177961eb08e812d2f1cbcc
Pedro Renan <pedroviana15094@gmail.com>
2024-05-24 17:21:07 -0300

    repositorio aula 08

--- src/main/java/academy/devdojo/springboot/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot/controller/AnimeController.java
@@ -7,7 +7,11 @@ import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,8 +30,16 @@ public class AnimeController {
 
     //localhost:8080/anime/list
     @GetMapping
-    public List<Anime> list(){
+    public ResponseEntity<List<Anime>> list(){
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now())); // exibe a localdatetime na execução do spring
-        return animeService.listAll();
+        return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK);
+        // Além de retornar a lista é interessante retornar algumas insformações sobre a consulta
+        // Utilizando ResponseEntity, conseguimos retornar mais algumas informações
+        // Como o status da requisição http(get)
     }
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id){
+        return ResponseEntity.ok(animeService.findById(id));
+    }
+
 }
--- src/main/java/academy/devdojo/springboot/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot/service/AnimeService.java
@@ -1,18 +1,29 @@
 package academy.devdojo.springboot.service;
 
 import academy.devdojo.springboot.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 // Geralmente responsalvel pela regra de negocios
 @Service
 public class AnimeService {
+    private List<Anime> animes = List.of(new Anime(1L,"One Piece"), new Anime(2L,"Berserk"),
+            new Anime(3L,"Boku No Hero"));
 
     // private final AnimeRepository animeRepository;
 
     public List<Anime> listAll(){
-        return List.of(new Anime(1L,"One Piece"), new Anime(2L,"Berserk"),
-                new Anime(3L,"Boku No Hero"));
+        return animes;
+
+    }
+
+    public Anime findById(long id){
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST,"Anime not Found"));
     }
 }
