ae883a9b549f9c26353d6e3724d94f823a0d5624
Pedro Renan <pedroviana15094@gmail.com>
2024-05-27 10:40:10 -0300

    repositorio aula13

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # springboot-essentials
 
 # Repositório voltado para o curso Spring Boot 2 Essentials DevDojo Academy
+
+- adicionado as dependencias no pom.xml, a dependencia do mysql e a spring data jpa
+- transfamando a anime domain em uma entidade do banco de dados com as annotations @Entity, @Id, @GeneratedValue, @NoArgsConstructor
+- adicionando o arquivo de conexão com o banco de dados
--- src/main/java/academy/devdojo/springboot/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot/domain/Anime.java
@@ -1,14 +1,23 @@
 package academy.devdojo.springboot.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data // o lombok cria os construtores e os getters e setters toString sozinho
 @AllArgsConstructor
 // O Jackson faz o mapeando da classe e compara se o nome dos atributos da classe são iguais aos nomes dos
 // atributos passados no arquivo JSON, se forem diferente o jackson vai ignorar
 // Caso eu queira renomear esse atributo para que seja reocnhecido eu tenho que por uma @JsonProperty("nomeAtt") com o nome do att no JSON
+@NoArgsConstructor
+@Entity
 public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
